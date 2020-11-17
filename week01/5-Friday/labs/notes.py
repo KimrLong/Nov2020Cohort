a6bfe1ac20ca12d3df591a0588a2c0e24fab2334
Kim Long <kimrlong09@gmail.com>
2020-11-17 10:07:44 -0500

    add

---
+++ week01/5-Friday/labs/notes.py
@@ -0,0 +1,20 @@
+def walk():
+    return "happy"
+def talk():
+    
+    return f"eating {meal}"
+def sleep():
+    foodList = ["ok", "lol", "pop"]
+    return foodList[0]
+def gym():
+    walk()
+    talk()
+    print("happy happy)")
+
+
+if True:
+    print('inside of if')
+
+    gym()
+
+  
