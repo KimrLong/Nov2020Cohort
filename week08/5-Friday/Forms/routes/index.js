345618fa9aa5be79805bcf1900d36406fc9a0961
Kim Long <kimrlong09@gmail.com>
2021-01-08 16:35:56 -0500

    forms

---
+++ week08/5-Friday/Forms/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+
+
+router.get('/', (req, res) =>{
+
+    res.render('index')
+})
+
+
+router.post('/', (req, res) =>{
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    console.log(req.body.fName, req.body.lName);
+    res.send(`${firstName} ${lastName}`);
+})
+
+
+
+module.exports = router;
